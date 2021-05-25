c5fd7d7dc012fb265beba53b0c6a574d4948be58
harsh bansal <harsh.bansal8849@gmail.com>
2021-05-24 21:16:13 -0400

    added passport and session, home link

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const https = require('https');
-const mongoose = require('mongoose');
-const md5 = require('md5');
-const _ = require('lodash');
-const ejs = require('ejs');
+const mongoose = require("mongoose");
+const _ = require("lodash");
+const ejs = require("ejs");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 const app = express();
 
 
@@ -14,13 +17,23 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
+app.use(session({
+  secret:"our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // connect with tha mongooseDB
 mongoose.connect("mongodb+srv://admin-harsh:Workpass1993@cluster0.uxnku.mongodb.net/maiwaeDB", {
    useUnifiedTopology: true,useNewUrlParser: true
 });
-// create schema and model that for new variables
-const userInfoSchema = {
-  email: String,
+mongoose.set("useCreateIndex",true);
+
+// added mongoose schema
+const userInfoSchema = new mongoose.Schema({
+  username: String,
   password: String,
   fName: String,
   lName: String,
@@ -39,18 +52,29 @@ const userInfoSchema = {
   city: String,
   state: String,
   zip: Number
-};
+});
+
+userInfoSchema.plugin(passportLocalMongoose);
+
 const User = mongoose.model("User", userInfoSchema);
 
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 const certSchema = {
-  name: String,
-  cover: String,
-  for: String
+  certNam: String,
+  certDes: String,
+  certReq: String
 };
 const courseSchema = {
-  name: String,
-  term: String,
-  major: String
+  degTyp: String,
+  degNam: String,
+  degElg: String,
+  degMjr: String,
+  degTrm: String,
+  degDes: String
 };
 const profileSchema = {
   name: String,
@@ -58,13 +82,33 @@ const profileSchema = {
   req2: String,
   req3: String
 };
+const examSchema = {
+  exNam: String,
+  exElg: String,
+  exPro: String
+};
+const compSchema = {
+  name: String,
+  cover: String,
+  for: String
+};
+const scholSchema = {
+  name: String,
+  cover: String,
+  for: String
+};
+const trendSchema = {
+  name: String,
+  cover: String,
+  for: String
+};
 const Certification = mongoose.model("Certification", certSchema);
-const Competition = mongoose.model("Competition", certSchema);
+const Competition = mongoose.model("Competition", compSchema);
 const Course = mongoose.model("Course", courseSchema);
-const Exam = mongoose.model("Exam", certSchema);
+const Exam = mongoose.model("Exam", examSchema);
 const Fprofile = mongoose.model("Fprofile", profileSchema);
-const Scholarship = mongoose.model("Scholarship", certSchema);
-const Trend = mongoose.model("Trend", certSchema);
+const Scholarship = mongoose.model("Scholarship", scholSchema);
+const Trend = mongoose.model("Trend", trendSchema);
 
 const ansSchema = {
   answer: String
@@ -81,77 +125,113 @@ const quesSchema = {
 };
 const Question = mongoose.model("Question", quesSchema);
 
-
-
-
-
-app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.uemail,
-    password: md5(req.body.upass),
-    fName: req.body.ufname,
-    lName: req.body.ulname,
-    sName: req.body.usclname,
-    sCourse: req.body.usclcours,
-    bDegree: req.body.ubachdeg,
-    bMajor: req.body.ubachmaj,
-    compName: req.body.ucompex,
-    compScore: req.body.ucompsc,
-    mDegree: req.body.umasdeg,
-    mMajor: req.body.umasmaj,
-    certification: req.body.ucert,
-    date: req.body.udobd,
-    month: req.body.udobm,
-    year: req.body.udoby,
-    city: req.body.ucity,
-    state: req.body.ustat,
-    zip: req.body.uzip
-  });
-  newUser.save(function(err) {
-    if (!err) {
-      res.render("signup");
-    }
-  });
-});
-
-
-
 app.get("/", function(req, res) {
   res.render("signup", );
 });
 
-app.post("/home", function(req, res) {
+app.get("/home",function(req,res){
+  if(req.isAuthenticated()){
+User.findById(req.user.id,function(err, foundUser){
 
-  const username = req.body.username;
-  const password = md5(req.body.password);
-  User.findOne({
-    email: username
-  }, function(err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        if (foundUser.password === password) {
-
-          res.render("home", {
-            fName: foundUser.fName,
-            bDegree: foundUser.bDegree,
-            bMajor: foundUser.bMajor,
-            compName: foundUser.compName,
-            compScore: foundUser.compScore,
-            mDegree: foundUser.mDegree,
-            mMajor: foundUser.mMajor,
-            certification: foundUser.certification,
-            sName: foundUser.sName,
-            sCourse: foundUser.sCourse
-          });
-        };
-      };
-    };
-  });
+res.render("home",{ fName:foundUser.fName, lName:foundUser.lName,sName:foundUser.sName, sCourse:foundUser.sCourse,
+  bDegree:foundUser.bDegree, bMajor:foundUser.bMajor, compName:foundUser.compName, compScore:foundUser.compScore,
+  mDegree:foundUser.mDegree, mMajor:foundUser.mMajor, certification:foundUser.certification,date:foundUser.date,
+  month:foundUser.month, year:foundUser.year, city:foundUser.city, state:foundUser.state, zip:foundUser.zip
+ });
+})
+  } else {
+    res.redirect("/");
+  }
 });
 
+app.get("/register",function(req,res){
+  res.render("register");
+});
 
+app.post("/register", function(req, res) {
+  User.register({username:req.body.username},req.body.password, function(err,user){
+    if(err){
+console.log(err);
+      res.redirect("/register");
+    } else{
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/");
+      })
+    }
+  })
+});
+
+app.post("/login", function(req, res) {
+  const user =  new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  req.login(user, function(err){
+    if(err){
+      console.log(err);
+    } else{
+      passport.authenticate("local")(req,res,function(){
+  res.redirect("/home");
+      })
+    }
+  })
+});
+
+app.get("/profile",function(req,res){
+  res.render("profile-page", );
+});
+
+app.post("/profile",function(req,res){
+console.log(req.user.id);
+  const fName= req.body.ufname;
+  const lName= req.body.ulname;
+  const sName= req.body.usclname;
+  const sCourse= req.body.usclcours;
+  const bDegree= req.body.ubachdeg;
+  const bMajor= req.body.ubachmaj;
+  const compName= req.body.ucompex;
+  const compScore= req.body.ucompsc;
+  const mDegree= req.body.umasdeg;
+  const mMajor= req.body.umasmaj;
+  const certification= req.body.ucert;
+  const date= req.body.udobd;
+  const month= req.body.udobm;
+  const year= req.body.udoby;
+  const city= req.body.ucity;
+  const state= req.body.ustat;
+  const zip= req.body.uzip;
+console.log(req.user.id);
+
+User.findById(req.user.id,function(err, foundUser){
+  if (err) {
+    console.log(err);
+  } else {
+    foundUser.fName= req.body.ufname;
+    foundUser.lName= req.body.ulname;
+    foundUser.sName= req.body.usclname;
+    foundUser.sCourse= req.body.usclcours;
+    foundUser.bDegree= req.body.ubachdeg;
+    foundUser.bMajor= req.body.ubachmaj;
+    foundUser.compName= req.body.ucompex;
+    foundUser.compScore= req.body.ucompsc;
+    foundUser.mDegree= req.body.umasdeg;
+    foundUser.mMajor= req.body.umasmaj;
+    foundUser.certification= req.body.ucert;
+    foundUser.date= req.body.udobd;
+    foundUser.month= req.body.udobm;
+    foundUser.year= req.body.udoby;
+    foundUser.city= req.body.ucity;
+    foundUser.state= req.body.ustat;
+    foundUser.zip= req.body.uzip;
+    foundUser.save(function(){
+      res.render("home",{
+        fName:fName, bDegree:bDegree, bMajor:bMajor, compName:compName, compScore:compScore,
+        mDegree:mDegree, mMajor:mMajor, certification: certification, sName:sName, sCourse: sCourse
+        });
+    })
+  }
+})
+});
 
 app.post("/competitions", function(req, res) {
   Competition.find({}, function(err, foundCerts) {
@@ -213,11 +293,6 @@ app.post("/trends", function(req, res) {
   });
 });
 
-// linked
-app.post("/profile", function(req, res) {
-  res.render("profile-page", );
-});
-
 app.post("/future", function(req, res) {
   Fprofile.find({}, function(err, foundCerts) {
     if (!err) {
@@ -258,8 +333,6 @@ app.get("/post", function(req, res)
                   });
   });
 
-
-
 app.post("/posts", function(req, res) {
   const newQues = new Question({
     ques: req.body.quesbtn
@@ -268,23 +341,16 @@ app.post("/posts", function(req, res) {
   res.redirect("post");
 });
 
-// make this like the todolist
-// abc answer for the xyz question
 app.post("/answer", function(req, res) {
-
 const reply = new Answer({answer: req.body.ansbtn});
-
 const questionId = req.body.questionId;
-
   console.log("reply sibmitted "+reply);
   console.log("question ID "+questionId);
 Question.findOne({_id:questionId},function(err,foundQ){
   if (!err) {
-
     foundQ.ans.push(reply);
     foundQ.save();
       }
-
 });
 res.redirect("post");
 });
@@ -298,7 +364,10 @@ const tP = req.body.trial;
 console.log(tP);
 });
 
-
+app.get("/logout",function(req,res){
+  req.logout();
+  res.redirect("/");
+})
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server running at port 3000");
